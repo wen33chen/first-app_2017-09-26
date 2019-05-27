2840c426c5a2cd22220b2325bd544048261fc71c
Chen Tsung Wen <wen33chen@me.com>
2019-05-27 15:50:51 +0800

    201905271530

--- src/sw.js
+++ src/sw.js
@@ -56,3 +56,47 @@ self.addEventListener('fetch', event => {
     .then(response => response || fetch(event.request))
   );
 });
+
+self.addEventListener('notificationclick', event => {
+  const notification = event.notification;
+  const action = event.action;
+  const link = notification.data.link;
+  const link_ok = notification.data.link_ok;
+  const link_ng = notification.data.link_ng;
+  switch (action) {
+    case 'yes':
+      if (link_ok) {
+        clients.openWindow(link_ok);
+      }
+      break;
+    case 'no':
+      if (link_ng) {
+        clients.openWindow(link_ng);
+      }
+      break;
+    case 'close':
+      break;
+    default:
+      if (link) {
+        clients.openWindow(link);
+      }
+      break;
+  }
+  notification.close();
+  console.log('notificationclick action is', action);
+})
+
+self.addEventListener('push', event => {
+  console.log('[Service Worker] Push Received.');
+  let title = 'Server Push';
+  let options = {
+    body: 'push TEST',
+    icon: './assets/images/android_048.png'
+  };
+  if (event.data) {
+    options = event.data.json();
+    title = options.title;
+  }
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
